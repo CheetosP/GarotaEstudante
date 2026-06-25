8258d32f995d15564d6c7acc753320b3e525652e
BuildTools <unconfigured@null.spigotmc.org>
2026-06-25 20:37:40 -0300

    Creating main loop Creating Menu and Game classes

--- GarotaEstudante/code/Menu.py
+++ GarotaEstudante/code/Menu.py
@@ -5,6 +5,8 @@ class Menu:
 
     def _init_(self, window):
         self.window = window
+        self.surf = pygame.image.load('./asset/menufoto.jpg')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
         pass
