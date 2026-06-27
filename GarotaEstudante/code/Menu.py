b5c6a62d01fe5a6854fa655c89998eac11875084
BuildTools <unconfigured@null.spigotmc.org>
2026-06-26 22:12:28 -0300

    Creating main loop Creating Menu and Game classes

--- GarotaEstudante/code/Menu.py
+++ GarotaEstudante/code/Menu.py
@@ -4,8 +4,7 @@
 import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
-
-from code.Const import WIN_WIDTH
+from code.Const import WIN_WIDTH, COLOR_GREEN, MENU_OPTION, COLOR_WHITE
 
 class Menu:
 
@@ -16,10 +15,14 @@ class Menu:
 
     def run(self):
         pygame.mixer_music.load('./asset/fundopantano.mp3')
-        pygame.mixer_music.play(-1)
+        #--------------------------------ygame.mixer_music.play(-1)
         while True:
-            self.menu_text(text_size=50, text="Garota Estudante", text_color=(255, 128, 0), text_center_pos=((WIN_WIDTH / 2), 70))
             self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(text_size=130, text="Rwby Aventureira", text_color=COLOR_GREEN, text_center_pos=((WIN_WIDTH / 2), 200))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(text_size=50, text=MENU_OPTION[i], text_color=COLOR_WHITE, text_center_pos=((WIN_WIDTH / 2), 350 + 50 * i))
+
             pygame.display.flip()
 
             # Check for all events
@@ -35,7 +38,7 @@ class Menu:
     #    self.window.blit(source=text_surf, dest=text_rect)
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
-            text_font: Font = pygame.font.SysFont("Lucida Sans Typewriter", text_size)
+            text_font: Font = pygame.font.SysFont("Impact", text_size)
             text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
             text_rect: Rect = text_surf.get_rect(center=text_center_pos)
             self.window.blit(source=text_surf, dest=text_rect)
